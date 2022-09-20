af4ee0e3fc0c84d7aa6a22f2b4d329935a84ed8c
nasirdiu <nasir.joypurhat@gmail.com>
2022-09-20 13:39:58 +0600

    client

--- components/Client/Client.js
+++ components/Client/Client.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import CountUp from "react-countup";
 const Client = () => {
   return (
     <div>
@@ -9,7 +9,51 @@ const Client = () => {
       <div className="flex justify-center">
         <progress className="progress  w-44 mt-2 bg-orange-700"></progress>
       </div>
-      
+      <div> 
+      <div className="bg-black text-white mt-5 md:px-32 ">
+        <div className="grid grid-cols-1 md:grid-cols-3 py-3 ">
+          <div className="count-title text-center bg-orange-500 p-5 w-2/4 rounded-md">
+            <div className="flex justify-center text-4xl font-bold">
+              <CountUp start={0} end={500} delay={0}>
+                {({ countUpRef }) => (
+                  <div>
+                    <span ref={countUpRef} />
+                  </div>
+                )}
+              </CountUp>
+              <p className="pl-2">+</p>
+            </div>
+            <h2 className="count-para">Happy Clients</h2>
+          </div>
+          <div className="count-title text-center bg-orange-500 p-5 w-2/4 rounded-md">
+            <div className="flex justify-center text-4xl font-bold">
+              <CountUp start={0} end={150} delay={0}>
+                {({ countUpRef }) => (
+                  <div>
+                    <span ref={countUpRef} />
+                  </div>
+                )}
+              </CountUp>
+              <p className="pl-2">+</p>
+            </div>
+            <h2 className="count-para">Current Clients</h2>
+          </div>
+          <div className="count-title text-center bg-orange-500 p-5 w-2/4 rounded-md">
+            <div className="flex justify-center text-4xl font-bold">
+              <CountUp start={0} end={1000} delay={0}>
+                {({ countUpRef }) => (
+                  <div>
+                    <span ref={countUpRef} />
+                  </div>
+                )}
+              </CountUp>
+              <p className="pl-2">+</p>
+            </div>
+            <h2 className="count-para">All Clients</h2>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,29 @@
-import Client from '../components/Client/Client'
-import Banner from '../components/Home/Banner'
-import Hotel from '../components/Hotel/Hotel'
-import Images from '../components/images/Images'
-import Review from '../components/Review/Review'
-import Scroll from '../components/Scroll/Scroll'
-import Footer from '../components/share/Footer'
-import Navbar from '../components/share/Navbar'
+import Client from "../components/Client/Client";
+import Banner from "../components/Home/Banner";
+import Hotel from "../components/Hotel/Hotel";
+import Images from "../components/images/Images";
+import Review from "../components/Review/Review";
+import Scroll from "../components/Scroll/Scroll";
+import Footer from "../components/share/Footer";
+import Navbar from "../components/share/Navbar";
 
 export default function Home() {
   return (
-    <div >
-          <Navbar/>
+    <div>
+      <Navbar />
 
-        <div className='bg-black'>
+      <div className="bg-black">
         <Banner></Banner>
-          <Images/>
-          <Hotel/>
-          <div className='bg-white'>
-          <Review/>
-          </div>
-          <Client/>
-          <Footer></Footer> 
-          <Scroll/>
+
+        <Images />
+        <Hotel />
+        <div className="bg-white">
+          <Review />
         </div>
-        
+        <Client />
+        <Footer></Footer>
+        <Scroll />
+      </div>
     </div>
-  )
+  );
 }
